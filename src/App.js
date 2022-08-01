cba45bff81ce069161f06f43a4f12f698c4252f3
Maaz Shah <s.tech4747@gmail.com>
2022-08-01 14:50:50 +0500

    added cart modal

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import { Fragment } from 'react';
+import Cart from './components/Cart/Cart';
 import Header from './components/Layout/Header'
 import Meals from './components/Meals/Meals';
+import Modal from './components/UI/Modal';
 
 function App() {
   return (
     <Fragment>
+      <Cart />
       <Header />
       <main>
         <Meals />
